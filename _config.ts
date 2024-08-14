e2cba729cea81c33db595b4506a2fa0fa0adf489
ooker <ganuongphap@gmail.com>
2024-08-14 21:13:37 +0700

    feat: thêm sitemap

--- _config.ts
+++ _config.ts
@@ -3,6 +3,8 @@ import wiki from "wiki/mod.ts";
 import metas from "lume/plugins/metas.ts";
 import ogImages from "lume/plugins/og_images.ts";
 import sitemap from "lume/plugins/sitemap.ts";
+import mermaid from "https://deno.land/x/lume_mermaid_plugin/mod.ts";
+
 const markdown = {
   options: {
     breaks: true,
@@ -17,6 +19,7 @@ const site = lume({
   },
 }, { markdown });
 
+
 site.use(wiki({
   favicon: {
     input: "assets/favicon.svg"
@@ -26,37 +29,6 @@ site.use(wiki({
 site.use(ogImages());
 site.use(metas());
 site.use(sitemap());
-
-function decodeEntities(encodedString: string) {
-  const translate_re = /&(nbsp|amp|quot|lt|gt);/g;
-  const translate = {
-    "nbsp":" ",
-    "amp" : "&",
-    "quot": "\"",
-    "lt"  : "<",
-    "gt"  : ">"
-  };
-  return encodedString.replace(translate_re, function(match, entity) {
-    return translate[entity];
-  }).replace(/&#(\d+);/gi, function(match, numStr) {
-    const num = parseInt(numStr, 10);
-    return String.fromCharCode(num);
-  });
-}
-
-site.process([".html"], (pages) => {
-  for (const page of pages) {
-    const mermaidBlocks = page.document?.getElementsByClassName("language-mermaid");
-    if (!mermaidBlocks) continue
-      for (const mermaidBlock of mermaidBlocks) {
-        mermaidBlock.className = "mermaid"
-        mermaidBlock.innerHTML = mermaidBlock.innerHTML.replaceAll("&gt;", ">").replaceAll("&lt;", "<")
-
-        // mermaidBlock.innerHTML = decodeEntities(mermaidBlock.innerHTML);
-      }
-    }
-  }
-);
+site.use(mermaid())
 
 export default site;
-
